c750625eb6022bce349bee61dd57c6ffc9b9463b
bratablessza <bratablessza1997@gmail.com>
2022-01-12 15:07:42 +0700

    update dashact,searchact,detaildosact,dosprofile'

---
+++ app/src/main/java/com/juaracoding/halodos/SearchActivity.java
@@ -0,0 +1,14 @@
+package com.juaracoding.halodos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
